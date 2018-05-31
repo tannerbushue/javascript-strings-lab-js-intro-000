2beec9a6a247e040d5087ea93c522b383f3b8f93
tannerbushue@gmail.com <tannerbushue@gmail.com>
2018-05-31 20:01:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space.";
+var topic = "space";
 
 var conversation = `Let's tak about ${topic}.`;
 
 console.log(greeting);
 console.log(greetSpecialGuest);
+console.log(conversation);
